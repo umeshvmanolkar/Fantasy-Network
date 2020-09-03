ffe1c3e91f51a54150abf2e3579f5e924b40493c
umesh <umeshvmanolkar@gmail.com>
2020-09-03 09:47:57 +0530

    Retrieving data from firebase and displaying it on home page.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'Authentication.dart';
 import 'PhotoUpload.dart';
+import 'Posts.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({
@@ -19,6 +21,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Posts> postList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    DatabaseReference postsRef =
+        FirebaseDatabase.instance.reference().child("Posts");
+    postsRef.once().then((DataSnapshot snap) {
+      var KEYS = snap.value.keys;
+      var DATA = snap.value;
+
+      postList.clear();
+
+      for (var individualKey in KEYS) {
+        Posts posts = new Posts(
+          DATA[individualKey]['image'],
+          DATA[individualKey]['description'],
+          DATA[individualKey]['date'],
+          DATA[individualKey]['time'],
+        );
+
+        postList.add(posts);
+      }
+
+      setState(() {
+        print('Length : $postList.length');
+      });
+    });
+  }
+
   void _logoutUser() async {
     try {
       await widget.auth.signOut();
@@ -35,7 +67,20 @@ class _HomePageState extends State<HomePage> {
       appBar: new AppBar(
         title: new Text("Home"),
       ),
-      body: new Container(),
+      body: new Container(
+        child: postList.length == 0
+            ? new Text("NO Posts are available")
+            : new ListView.builder(
+                itemBuilder: (_, index) {
+                  return PostUI(
+                      postList[index].image,
+                      postList[index].description,
+                      postList[index].date,
+                      postList[index].time);
+                },
+                itemCount: postList.length,
+              ),
+      ),
       bottomNavigationBar: new BottomAppBar(
         color: Colors.deepOrange,
         child: new Container(
@@ -62,6 +107,53 @@ class _HomePageState extends State<HomePage> {
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget PostUI(
+    String image,
+    String description,
+    String date,
+    String time,
+  ) {
+    return new Card(
+      elevation: 10.0,
+      margin: EdgeInsets.all(15.0),
+      child: new Container(
+        padding: new EdgeInsets.all(14.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Text(
+                  date,
+                  style: Theme.of(context).textTheme.subtitle,
+                  textAlign: TextAlign.center,
+                ),
+                new Text(
+                  time,
+                  style: Theme.of(context).textTheme.subtitle,
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            new Image.network(image, fit: BoxFit.cover),
+            SizedBox(
+              height: 10.0,
+            ),
+            new Text(
+              description,
+              style: Theme.of(context).textTheme.subhead,
+              textAlign: TextAlign.center,
+            )
+          ],
         ),
       ),
     );
